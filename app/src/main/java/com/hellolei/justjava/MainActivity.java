b9bf3216c637acfb02487e5605eb529cc763338a
Lei Yu <lei.leiy@gmail.com>
2016-06-14 00:31:46 -0700

    Udacity Just Java Lesson 3

---
+++ app/src/main/java/com/hellolei/justjava/MainActivity.java
@@ -0,0 +1,193 @@
+package com.hellolei.justjava;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        EditText customerNameInputField = (EditText)findViewById(R.id.customer_name_view);
+        String customerName = customerNameInputField.getText().toString();
+        //Log.v("Main Activity", "Customer Name:" + customerName);
+
+        CheckBox whippedCreamCheckbox = (CheckBox)findViewById(R.id.whipped_cream_checkout);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+       // Log.v("Main Activity", "Has whipped cream:" + hasWhippedCream);
+
+
+        CheckBox chocolateCheckbox = (CheckBox)findViewById(R.id.chocolate_checkout);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+        //Log.v("Main Activity", "Has chocolate:" + hasChocolate);
+
+
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, customerName);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order for " + customerName);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Calculates the price of the order
+     *
+     * @param addWhippedCream is whether user wants whipped cream topping
+     * @param addChocolate    is whether user wants chocolate topping
+     */
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Price of 1 cup of coffee
+        int basePrice = 5;
+
+        // Add $1 if user wants whipped cream
+        if (addWhippedCream) {
+            basePrice += 1;
+            //Log.v("Main Activity", "Has whipped cream new price:" + basePrice);
+        }
+
+        // Add $2 if user wants chocolate
+        if (addChocolate) {
+            basePrice += 2;
+            //Log.v("Main Activity", "Has chocolate new price:" + basePrice);
+        }
+
+        // Calculate total order price
+        return quantity * basePrice;
+    }
+
+    /**
+     * This method is called when the + button is clicked.
+     */
+    public void increment(View view) {
+
+        if (quantity >= 100) {
+            Toast.makeText(this, "Call store to order over 100 cups", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            quantity += 1;
+            displayQuantity(quantity);
+        }
+        ;
+    }
+
+    /**
+     * This method is called when the - button is clicked.
+     */
+    public void decrement(View view) {
+        if (quantity < 1) {
+            Toast.makeText(this, "No coffee ordered", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            quantity -= 1;
+            displayQuantity(quantity);
+        }
+        ;
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayQuantity(int numberofCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + numberofCoffees);
+    }
+
+
+
+    /**
+     * This method is called when the order button is clicked to create a summary of the order.
+     *
+     * @param price           of the order
+     * @param addWhippedCream is whether or not the user wants whipped cream
+     * @return text summary
+     */
+
+    private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String customerName) {
+        String priceMessage = customerName;
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\n Quantity: " + quantity;
+        priceMessage += "\nTotal: $" + price;
+        priceMessage += "\nThank You!";
+        return priceMessage;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.hellolei.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.hellolei.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
